4abfce20bced5d01ca654c7e4fce91a455ebb18c
Jyotisman17 <jyotisman.behera.17@gmail.com>
2022-11-23 23:28:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import PreNavBar from './components/PreNavBar';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router} from "react-router-dom";
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+import Offers from "./components/Offers.js"
+import Heading from "./components/Heading.js"
+import StarProduct from"./components/StarProduct.js"
+
+
+
+function App() {
+  return (
+  <Router>
+    <PreNavBar/>
+    <NavBar />
+    <Slider start={data.banner.start} />
+    <Offers offers={data.offer}/>
+    <Heading text="STAR PRODUCTS"/>
+    <StarProduct starProduct={data.starProduct}/>
+    <Heading text="HOT PRODUCTS"/>
+    <Heading text="This site is under construction  "/>
+    <Heading text="Guided by K.L.Narayan Sir"/>
+  </Router>
+  );
+}
+
+export default App;
